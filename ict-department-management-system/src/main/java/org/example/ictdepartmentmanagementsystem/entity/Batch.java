2841d052178a2e3b9c3a18cd294418b55020a257
Hashan Dileepa <hashdileepa@gmail.com>
2026-07-15 01:19:29 +0530

    Update Batch.java

--- ict-department-management-system/src/main/java/org/example/ictdepartmentmanagementsystem/entity/Batch.java
+++ ict-department-management-system/src/main/java/org/example/ictdepartmentmanagementsystem/entity/Batch.java
@@ -1,8 +1,15 @@
 package org.example.ictdepartmentmanagementsystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Batch {
     BATCH_2021,
     BATCH_2022,
     BATCH_2023,
-    BATCH_2024
+    BATCH_2024;
+
+    @JsonCreator
+    public static Batch fromString(String value) {
+        return Batch.valueOf(value.toUpperCase());
+    }
 }
