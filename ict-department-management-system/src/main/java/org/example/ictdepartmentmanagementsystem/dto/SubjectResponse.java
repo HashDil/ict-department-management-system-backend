727dfaae07d67633ab1079a34087e511f6a1b25b
Hashan Dileepa <hashdileepa@gmail.com>
2026-07-19 01:55:23 +0530

    Subject service  added

--- ict-department-management-system/src/main/java/org/example/ictdepartmentmanagementsystem/dto/SubjectResponse.java
+++ ict-department-management-system/src/main/java/org/example/ictdepartmentmanagementsystem/dto/SubjectResponse.java
@@ -1,15 +1,16 @@
 package org.example.ictdepartmentmanagementsystem.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class ResultResponse {
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubjectResponse {
 
     private String subjectCode;
     private String subjectName;
     private int creditHours;
-    private double marks;
-    private String grade;
-    private double gradePoint;
     private String semester;
 }
---
+++ ict-department-management-system/src/main/java/org/example/ictdepartmentmanagementsystem/service/SubjectService.java
@@ -0,0 +1,58 @@
+package org.example.ictdepartmentmanagementsystem.service;
+
+import org.example.ictdepartmentmanagementsystem.dto.AddSubjectRequest;
+import org.example.ictdepartmentmanagementsystem.dto.SubjectResponse;
+import org.example.ictdepartmentmanagementsystem.entity.Semester;
+import org.example.ictdepartmentmanagementsystem.entity.Subject;
+import org.example.ictdepartmentmanagementsystem.repository.SubjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
+    public SubjectService(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    public SubjectResponse addSubject(AddSubjectRequest request) {
+        if(subjectRepository.existsBySubjectCode(request.getSubjectCode())) {
+            throw new IllegalArgumentException("Subject code already exists");
+        }
+        Subject subject = new Subject();
+        subject.setSubjectCode(request.getSubjectCode());
+        subject.setSubjectName(request.getSubjectName());
+        subject.setCreditHours(request.getCreditHours());
+        subject.setSemester(request.getSemester());
+
+        return mapToResponse(subjectRepository.save(subject));
+    }
+
+    public List<SubjectResponse>getAllSubjects() {
+        return subjectRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public List<SubjectResponse> getSubjectsBySemester(Semester semester) {
+        return subjectRepository.findBySemester(semester)
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public SubjectResponse mapToResponse(Subject subject) {
+        SubjectResponse subjectResponse = new SubjectResponse();
+        subjectResponse.setSubjectCode(subject.getSubjectCode());
+        subjectResponse.setSubjectName(subject.getSubjectName());
+        subjectResponse.setCreditHours(subject.getCreditHours());
+        subjectResponse.setSemester(subject.getSemester().name());
+
+        return subjectResponse;
+    }
+}
